bab2c5a1b755c12f354dcc6300dd729a6b5eac98
0xFlo <floriankluge@gmail.com>
2024-10-21 07:54:35 +0100

    add ChatRoomLive

--- config/test.exs
+++ config/test.exs
@@ -6,12 +6,11 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :slax, Slax.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "slax_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "priv/repo/slax_test#{System.get_env("MIX_TEST_PARTITION")}.db",
   pool: Ecto.Adapters.SQL.Sandbox,
-  pool_size: System.schedulers_online() * 2
+  pool_size: System.schedulers_online() * 2,
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: true
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
