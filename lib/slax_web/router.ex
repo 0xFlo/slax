208bda700ee679af519afcf64368506754d1e5d7
0xFlo <floriankluge@gmail.com>
2024-10-22 10:01:03 +0100

    optimized docs

--- lib/slax_web/router.ex
+++ lib/slax_web/router.ex
@@ -25,18 +25,8 @@ defmodule SlaxWeb.Router do
     live "/accounts", UserListLive, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", SlaxWeb do
-  #   pipe_through :api
-  # end
-
   # Enable LiveDashboard and Swoosh mailbox preview in development
   if Application.compile_env(:slax, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
 
     scope "/dev" do
@@ -48,7 +38,6 @@ defmodule SlaxWeb.Router do
   end
 
   ## Authentication routes
-
   scope "/", SlaxWeb do
     pipe_through [:browser, :redirect_if_user_is_authenticated]
 
