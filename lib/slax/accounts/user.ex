f5e9005417f6c22f0ce7f0ca00d825a6dd9b654c
0xFlo <floriankluge@gmail.com>
2024-10-23 16:11:10 +0100

    add context functions to create and update rooms

--- lib/slax/accounts/user.ex
+++ lib/slax/accounts/user.ex
@@ -2,6 +2,8 @@ defmodule Slax.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Slax.Repo
+
   @hash_opts [hash_password: true, validate_email: true]
 
   schema "users" do
@@ -15,9 +17,6 @@ defmodule Slax.Accounts.User do
     timestamps(type: :utc_datetime)
   end
 
-  @doc """
-  A changeset for user registration, including optional password hashing and email uniqueness validation.
-  """
   def registration_changeset(user, attrs, opts \\ @hash_opts) do
     user
     |> cast(attrs, [:username, :email, :password])
@@ -27,9 +26,6 @@ defmodule Slax.Accounts.User do
     |> validate_password(opts)
   end
 
-  @doc """
-  A changeset for changing the username, ensuring it is unique.
-  """
   def username_changeset(user, attrs) do
     user
     |> cast(attrs, [:username])
@@ -37,38 +33,28 @@ defmodule Slax.Accounts.User do
     |> validate_username()
   end
 
-  @doc """
-  A changeset for changing the email, ensuring that the email actually changes.
-  """
   def email_changeset(user, attrs, opts \\ @hash_opts) do
     user
     |> cast(attrs, [:email])
+    |> validate_required([:email])
     |> validate_email(opts)
     |> validate_change(:email, &email_change_required/2)
   end
 
-  @doc """
-  A changeset for changing the password with optional hashing.
-  """
   def password_changeset(user, attrs, opts \\ @hash_opts) do
     user
     |> cast(attrs, [:password])
+    |> validate_required([:password])
     |> validate_confirmation(:password, message: "does not match password")
     |> validate_password(opts)
   end
 
-  @doc """
-  Confirms a user's account by setting `confirmed_at`.
-  """
   def confirm_changeset(user) do
     change(user, confirmed_at: DateTime.utc_now() |> DateTime.truncate(:second))
   end
 
-  @doc """
-  Validates the current password or adds an error to the changeset.
-  """
   def validate_current_password(changeset, password) do
-    changeset = cast(changeset, %{current_password: password}, [:current_password])
+    changeset = put_change(changeset, :current_password, password)
 
     if valid_password?(changeset.data, password) do
       changeset
@@ -77,9 +63,6 @@ defmodule Slax.Accounts.User do
     end
   end
 
-  @doc """
-  Verifies if the given password matches the stored hashed password.
-  """
   def valid_password?(%__MODULE__{hashed_password: hashed_password}, password)
       when is_binary(hashed_password) and byte_size(password) > 0 do
     Bcrypt.verify_pass(password, hashed_password)
@@ -87,12 +70,12 @@ defmodule Slax.Accounts.User do
 
   def valid_password?(_, _), do: Bcrypt.no_user_verify() && false
 
-  # Private helpers
+  # Private helper functions
 
   defp validate_username(changeset) do
     changeset
     |> validate_length(:username, min: 3, max: 20)
-    |> unsafe_validate_unique(:username, Slax.Repo)
+    |> unsafe_validate_unique(:username, Repo)
     |> unique_constraint(:username)
   end
 
@@ -115,7 +98,7 @@ defmodule Slax.Accounts.User do
   defp maybe_validate_unique_email(changeset, opts) do
     if Keyword.get(opts, :validate_email, true) do
       changeset
-      |> unsafe_validate_unique(:email, Slax.Repo)
+      |> unsafe_validate_unique(:email, Repo)
       |> unique_constraint(:email)
     else
       changeset
