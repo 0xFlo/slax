ff5f65416617bdad1e78bff9d44592af9afa2479
0xFlo <floriankluge@gmail.com>
2024-10-31 09:00:01 +0000

    convert user_list_live to streams

--- lib/slax_web/live/user_list_live.ex
+++ lib/slax_web/live/user_list_live.ex
@@ -1,46 +1,32 @@
 defmodule SlaxWeb.UserListLive do
   use SlaxWeb, :live_view
-
   alias Slax.Repo
   alias Slax.Accounts.User
 
   def mount(_params, _session, socket) do
     users = Repo.all(User)
-
-    {:ok, assign(socket, users: users)}
+    {:ok, socket |> stream(:users, users)}
   end
 
   def render(assigns) do
     ~H"""
     <.header class="text-center">All Registered Users</.header>
-    <.table id="user-list" rows={@users}>
-      <:col :let={user} label="ID"><%= user.id %></:col>
-      <:col :let={user} label="Username">
+    <.table id="users" rows={@streams.users}>
+      <:col :let={{_id, user}} label="ID"><%= user.id %></:col>
+      <:col :let={{_id, user}} label="Username">
         <%= if user.username do %>
-          <%= user.username %>
-        <% else %>
-          <span class="text-zinc-400 text-sm">Not set</span>
-        <% end %>
-      </:col>
-      <:col :let={user} label="Profile">
-        <%= if user.bio || user.location || user.website do %>
           <.link
             navigate={~p"/profiles/#{user.username}"}
             class="text-blue-600 hover:text-blue-700 hover:underline"
           >
-            @<%= user.username %>
+            <%= user.username %>
           </.link>
         <% else %>
-          <.link
-            navigate={~p"/profiles/#{user.username}/edit"}
-            class="text-zinc-500 hover:text-zinc-700 text-sm hover:underline"
-          >
-            → Create profile
-          </.link>
+          <span class="text-zinc-400 text-sm">Not set</span>
         <% end %>
       </:col>
-      <:col :let={user} label="Email"><%= user.email %></:col>
-      <:col :let={user} label="Confirmed At">
+      <:col :let={{_id, user}} label="Email"><%= user.email %></:col>
+      <:col :let={{_id, user}} label="Confirmed At">
         <%= user.confirmed_at || "Not confirmed" %>
       </:col>
     </.table>
